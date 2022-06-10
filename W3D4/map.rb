768061baf596f20c5c6528d970707e0ab6ca59ad
Diana <mada.chirica@gmail.com>
2022-06-09 22:07:52 -0400

    Attempted Map ADT

--- W3D4/map.rb
+++ W3D4/map.rb
@@ -1,21 +1,39 @@
 class Map
     attr_reader :items
+
     def initialize
         @items = []
       
     end
 
     def set(key, value)
-        @items << [key, value]
+        @items.each {|pair| @items.shift([key, value]) if pair != [key, value]}
      
     end
 
     def get(key)
-
-      
+        @items.each do |pair| 
+            if pair[first] == key
+                @return pair.first 
+            else
+                return nil
+            end
+        end
     end
 
     def delete(key)
+        @items.each do |pair| 
+            if pair[first] == key
+                @items.pop(pair) 
+            else
+                @items
+            end
+        end
+
+    end
+
+    def show
+        return @items
       
     end
   end
