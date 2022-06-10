9a6b431d00c9065fd97f9fa3168ab236f763d3b9
Diana <mada.chirica@gmail.com>
2022-06-09 21:42:20 -0400

    Created Queue

---
+++ W3D4/queue.rb
@@ -0,0 +1,17 @@
+class Queue
+    def initialize
+      @items
+    end
+
+    def enqueue(el)
+      @items.unshift(el)
+    end
+
+    def dequeue
+      @items.pop
+    end
+
+    def peek
+      return @items.last
+    end
+  end
